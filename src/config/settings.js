5fd051c2f68beb0d2c3b983826f3f0f6961be37e
Facu Rossi <rossifacundo94@gmail.com>
2018-11-04 21:10:58 -0300

    Update settings

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://api.openweathermap.org/data/2.5/weather";
+export const API_URL = "https://api.openweathermap.org/data/2.5/weather";
 export const API_KEY = "76cf30cbc981c111a63f908d867399ce";
 export const MAX_CITIES_ALLOWED = 5;
 export const GOOGLE_MAP_API_KEY = "AIzaSyD6kixofLg4seM7aWKn9C7Il6_HCaCZxi0";
