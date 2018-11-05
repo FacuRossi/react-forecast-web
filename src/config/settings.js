b239460fc993fc25e6b6bb840dc05a63bf79d3f6
Facu Rossi <rossifacundo94@gmail.com>
2018-11-04 23:42:09 -0300

    Update Settings

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,5 @@
 export const API_URL = "https://api.openweathermap.org/data/2.5/weather";
 export const API_KEY = "76cf30cbc981c111a63f908d867399ce";
 export const MAX_CITIES_ALLOWED = 5;
-export const GOOGLE_MAP_API_KEY = "AIzaSyD6kixofLg4seM7aWKn9C7Il6_HCaCZxi0";
+export const GOOGLE_MAP_API_KEY = "AIzaSyBihOZg8fKdhszGNPGdFFZrj7avdjTEYTg";
 export const GOOGLE_MAP_DEFAULT_ZOOM = 11;
-//AIzaSyBihOZg8fKdhszGNPGdFFZrj7avdjTEYTg
